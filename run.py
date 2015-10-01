9affeb048652323cb51d0e434b83aaeab8838b2a
Pierre Jaury <pierre@jaury.eu>
2015-10-01 11:18:06 +0200

    Add a license at twitter's request

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 #!/usr/local/bin/python3
+#
+# "THE BEER-WARE LICENSE" (Revision 42):
+# kaiyou wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return.
+#
 
 import docker
 import jinja2
